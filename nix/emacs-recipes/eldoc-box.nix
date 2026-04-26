d6a12756853a8d10c85f2d705ff077a397b3a96a
Пётр <11111000000@email.com>
2026-04-26 18:53:20 +0800

    chore: noop

--- nix/emacs-recipes/eldoc-box.nix
+++ nix/emacs-recipes/eldoc-box.nix
@@ -4,10 +4,10 @@ emacsPackages.buildEmacsPackage rec {
   pname = "eldoc-box";
   version = "0.3.0";
   src = fetchFromGitHub {
-    owner = "vitalie";
+    owner = "casouri";
     repo = "eldoc-box";
-    rev = "v0.3.0";
-    sha256 = "0000000000000000000000000000000000000000000000000000"; # placeholder
+    rev = "2680a08ff2438ff8c2ea6f8d57f22095f857900c";
+    sha256 = "1iqha79lpydaz2i5dah11zsj060bs1livl3fpi76kh3j5ak6v5id";
   };
   meta = with stdenv.lib; {
     description = "Show eldoc in a childframe/posframe";
