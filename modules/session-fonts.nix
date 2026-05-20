037d10e103418e0dcc85c72c89713da2d0bed828
Пётр <11111000000@email.com>
2026-05-20 19:54:38 +0800

    fix: adjust font configuration paths after module relocation

--- modules/session-fonts.nix
+++ modules/session-fonts.nix
@@ -21,13 +21,13 @@
     monospace = [ "Aporetic Sans Mono" "Terminus" ];
   };
 
-  environment.etc."fonts.conf".source = ../../conf/fonts.conf;
-  environment.etc."gtk-3.0/settings.ini".source = ../../conf/gtk-3.0-settings.ini;
-  environment.etc."gtk-4.0/settings.ini".source = ../../conf/gtk-4.0-settings.ini;
-  environment.etc."gtk-2.0/gtkrc".source = ../../conf/gtkrc-2.0;
-  environment.etc."xdg/qt5ct/qt5ct.conf".source = ../../conf/qt5ct.conf;
-  environment.etc."xdg/qt6ct/qt6ct.conf".source = ../../conf/qt6ct.conf;
-  environment.etc."xdg/kdeglobals".source = ../../conf/kdeglobals;
-  environment.etc."X11/Xresources".text = builtins.readFile ../../conf/Xresources;
-  environment.etc."xdg/dunst/dunstrc".source = ../../conf/dunstrc;
+  environment.etc."fonts.conf".source = ../conf/fonts.conf;
+  environment.etc."gtk-3.0/settings.ini".source = ../conf/gtk-3.0-settings.ini;
+  environment.etc."gtk-4.0/settings.ini".source = ../conf/gtk-4.0-settings.ini;
+  environment.etc."gtk-2.0/gtkrc".source = ../conf/gtkrc-2.0;
+  environment.etc."xdg/qt5ct/qt5ct.conf".source = ../conf/qt5ct.conf;
+  environment.etc."xdg/qt6ct/qt6ct.conf".source = ../conf/qt6ct.conf;
+  environment.etc."xdg/kdeglobals".source = ../conf/kdeglobals;
+  environment.etc."X11/Xresources".text = builtins.readFile ../conf/Xresources;
+  environment.etc."xdg/dunst/dunstrc".source = ../conf/dunstrc;
 }
