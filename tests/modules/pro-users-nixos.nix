b8000211b33e633618889664da94cc17282dc7dd
Пётр <11111000000@email.com>
2026-05-23 17:28:07 +0800

    tests: пробросить pro-users-nixos в пространство tests для VM-конфигов

---
+++ tests/modules/pro-users-nixos.nix
@@ -0,0 +1,35 @@
+{ config, lib, pkgs, opencodeBwrapModule ? null, ... }:
+
+{
+  home-manager.users = builtins.listToAttrs (map (name: {
+    inherit name;
+    value = {
+      imports = [ ../emacs/home-manager.nix ] ++ lib.optionals (opencodeBwrapModule != null) [ opencodeBwrapModule ];
+      home.username = name;
+      home.homeDirectory = "/home/${name}";
+      home.stateVersion = "23.11";
+      pro.emacs = {
+        enable = true;
+        gui.enable = false;
+        providedPackages = [
+          "ace-window" "avy" "cape" "consult" "consult-dash" "consult-eglot" "consult-projectile" "consult-yasnippet"
+          "corfu" "corfu-posframe" "corfu-terminal" "dash-docs" "eglot" "elfeed" "expand-region" "gptel"
+          "kind-icon" "magit" "marginalia" "nix-mode" "orderless" "org" "projectile" "rainbow-delimiters"
+          "treemacs" "vertico" "vterm" "yasnippet" "embark-consult" "dash-docs" "consult-dash"
+        ];
+
+        extraPackages = let
+          piAcp = if builtins.hasAttr "piAcp" pkgs then [ pkgs.piAcp ] else [ ];
+        in piAcp ++ [
+          pkgs.emacsPackages.ace-window pkgs.emacsPackages.avy pkgs.emacsPackages.cape pkgs.emacsPackages.consult
+          pkgs.emacsPackages.consult-dash pkgs.emacsPackages.consult-eglot pkgs.emacsPackages.consult-projectile pkgs.emacsPackages.consult-yasnippet
+          pkgs.emacsPackages.corfu pkgs.emacsPackages.dash-docs pkgs.emacsPackages.consult-dash pkgs.emacsPackages.embark-consult
+          pkgs.emacsPackages.eglot pkgs.emacsPackages.elfeed pkgs.emacsPackages.expand-region pkgs.emacsPackages.gptel
+          pkgs.emacsPackages.kind-icon pkgs.emacsPackages.magit pkgs.emacsPackages.marginalia pkgs.emacsPackages.nix-mode
+          pkgs.emacsPackages.orderless pkgs.emacsPackages.org pkgs.emacsPackages.projectile pkgs.emacsPackages.rainbow-delimiters
+          pkgs.emacsPackages.treemacs pkgs.emacsPackages.vertico pkgs.emacsPackages.vterm pkgs.emacsPackages.yasnippet
+        ];
+      };
+    };
+  }) [ "az" "za" "la" "bo" ]);
+}
