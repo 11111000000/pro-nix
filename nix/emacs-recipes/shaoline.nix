f2160a17fc63389a101ede666f3fd5997fb8758f
Пётр <11111000000@email.com>
2026-06-02 01:12:49 +0800

    nix: add shaoline emacs recipe and expose in overlays; mark as provided package

---
+++ nix/emacs-recipes/shaoline.nix
@@ -0,0 +1,31 @@
+{ stdenv, emacs, lib }:
+
+stdenv.mkDerivation rec {
+  pname = "shaoline";
+  version = "0.0.0";
+  src = ../../submodules/shaoline;
+
+  nativeBuildInputs = [ emacs ];
+  dontConfigure = true;
+
+  buildPhase = ''
+    runHook preBuild
+    # Compile Elisp files located in lisp/
+    emacs --batch -Q -L lisp -f batch-byte-compile lisp/*.el 2>/dev/null || true
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/share/emacs/site-lisp/${pname}
+    cp lisp/*.el $out/share/emacs/site-lisp/${pname}/
+    cp lisp/*.elc $out/share/emacs/site-lisp/${pname}/ 2>/dev/null || true
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "shaoline modeline (11111000000/shaoline)";
+    homepage = "https://github.com/11111000000/shaoline";
+    license = licenses.mit;
+  };
+}
